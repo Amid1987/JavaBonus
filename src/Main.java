d3384cac058f7a9cf65b82a84833c7c8e7d1df83
Dmitrii <fsb2100_1987@mail.ru>
2022-09-13 19:47:37 +0300

    add JavaBonus

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        int threshold = 1000;
+        int replenishmentAmount = 700;
+        int currentSum = 400;
+        int finalScore = replenishmentAmount + currentSum;
+        int bonus;
+
+        if (finalScore > threshold) {
+            bonus = (finalScore - replenishmentAmount) / 100;
+            {
+            }
+        } else {
+            bonus = 0;
+        }
+
+        if (replenishmentAmount <= 1000) {
+            bonus = (replenishmentAmount + currentSum - threshold) / 100;
+            if (bonus <= 0) System.out.println("Ваш баланс равен " + finalScore);
+            if (bonus > 0) {
+                System.out.println("Ваш бонус равен " + bonus);
+                System.out.println("Ваш баланс равен " + finalScore);
+
+            }
+        }
+
+
+    }
+}
