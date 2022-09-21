63ecbb191afd27bfa087e447fd23dec38254219d
Dmitrii <fsb2100_1987@mail.ru>
2022-09-21 20:49:19 +0300

    New2JavaBonus

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         int threshold = 1000;
-        int replenishmentAmount = 700;
+        int replenishmentAmount = 10000;
         int currentSum = 400;
         int finalScore = replenishmentAmount + currentSum;
         int bonus;
 
         if (finalScore > threshold) {
-            bonus = (finalScore - replenishmentAmount) / 100;
+            bonus = (finalScore - threshold) / 100;
+            System.out.println("Ваш баланс равен " + finalScore);
+            System.out.println("Ваш бонус равен " + bonus);
         } else {
             bonus = 0;
+            System.out.println("Ваш баланс равен " + finalScore);
         }
-        if (replenishmentAmount <= 1000) {
+        if (replenishmentAmount >= 1000) {
             bonus = (replenishmentAmount + currentSum - threshold) / 100;
             if (bonus <= 0) {
                 System.out.println("Ваш баланс равен " + finalScore);
-            } else {
                 System.out.println("Ваш бонус равен " + bonus);
-                System.out.println("Ваш баланс равен " + finalScore);
             }
         }
     }
